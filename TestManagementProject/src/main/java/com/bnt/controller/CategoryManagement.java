ded393812ab7c5314175e70fb6a52201e9726737
rohini.khod <rohini.khod@ROHINIKHOD.bnt.local>
2024-02-07 23:12:21 +0530

    Changed Name for CategoryController

---
+++ TestManagementProject/src/main/java/com/bnt/controller/CategoryManagement.java
@@ -0,0 +1,85 @@
+package com.bnt.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bnt.exception.CategoryNotFoundException;
+import com.bnt.model.CategoryRequest;
+import com.bnt.model.CategoryResponse;
+import com.bnt.service.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryManagement {
+
+	private static final Logger logger = LoggerFactory.getLogger(CategoryManagement.class);
+
+	@Autowired
+	private CategoryService service;
+
+	@PostMapping("/insert")
+	public CategoryRequest addNewCategory(@RequestBody CategoryRequest category) {
+
+		CategoryRequest addcategory = service.addNewCategory(category);
+		logger.info("Added a new category: {}", category);
+		return addcategory;
+
+	}
+
+	@GetMapping
+	public List<CategoryResponse> getAllCategory() {
+
+		List<CategoryResponse> showCategories = service.getAllCatogory();
+		logger.info("Getting all categories: {}", showCategories.toString());
+		return showCategories;
+	}
+
+	@GetMapping("/{category_id}")
+	public ResponseEntity<?> getCategoryById(@PathVariable("category_id") Long categoryId) {
+		try {
+			CategoryResponse category = service.getCategoryById(categoryId);
+			logger.info("Getting category by ID: {}", category);
+			return ResponseEntity.ok(category);
+		} catch (CategoryNotFoundException ex) {
+			logger.error("Category not found with ID: {}", categoryId, ex);
+			throw new CategoryNotFoundException("Error occurred while getById category" + ex);
+		}
+	}
+
+	@PutMapping
+	public ResponseEntity<CategoryResponse> updateCategory(@RequestBody CategoryRequest category) {
+		try {
+			CategoryResponse updatedCategory = service.updateCategory(category);
+			return ResponseEntity.ok(updatedCategory);
+		} catch (CategoryNotFoundException e) {
+			logger.error("Error occurred while updating category", e);
+			throw new CategoryNotFoundException("Error occurred while updating category" + e);
+		}
+	}
+
+	@DeleteMapping("/{category_id}")
+	public ResponseEntity<?> deleteCategory(@PathVariable("category_id") Long categoryId) {
+		try {
+			logger.info("Deleting category with ID: {}", categoryId);
+			this.service.deleteCategory(categoryId);
+			return ResponseEntity.ok().build();
+		} catch (CategoryNotFoundException ex) {
+			logger.error("Category not found with ID: {}", categoryId, ex);
+			throw new CategoryNotFoundException("Error occurred while Deleting category" + ex);
+		}
+
+	}
+
+}
---
+++ TestManagementProject/src/test/java/com/bnt/controller/CategoryManagementTest.java
@@ -0,0 +1,145 @@
+package com.bnt.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.bnt.exception.CategoryNotFoundException;
+import com.bnt.model.CategoryRequest;
+import com.bnt.model.CategoryResponse;
+import com.bnt.service.CategoryService;
+
+@SpringBootTest
+class CategoryManagementTest {
+
+	@Mock
+	private CategoryService categoryService;
+
+	@InjectMocks
+	private CategoryManagement categories;
+
+	public CategoryRequest categoryData() {
+		CategoryRequest category = new CategoryRequest();
+		category.setCategoryId(1L);
+		category.setTitle("Spring Core");
+		category.setDescription("This is Spring Core Category Created");
+		return category;
+	}
+
+	@Test
+	public void testAddNewCategory() {
+
+		CategoryRequest category = categoryData();
+
+		when(categoryService.addNewCategory(any(CategoryRequest.class))).thenReturn(category);
+
+		CategoryRequest result = categories.addNewCategory(category);
+
+		assertEquals(category, result);
+	}
+
+	@Test
+	public void testGetAllCategory() {
+		List<CategoryResponse> mockCategories = new ArrayList<>();
+		CategoryRequest category = categoryData();
+		mockCategories.add(category.toResponse());
+
+		when(categoryService.getAllCatogory()).thenReturn(mockCategories);
+
+		List<CategoryResponse> result = categories.getAllCategory();
+
+		assertNotNull(result);
+		assertEquals(mockCategories, result);
+	}
+
+	@Test
+	public void testGetCategoryById_Success() {
+
+		Long categoryId = 1L;
+		CategoryResponse mockCategory = new CategoryResponse(categoryId, "Spring Core",
+				"This is Spring Core Category Created");
+		when(categoryService.getCategoryById(categoryId)).thenReturn(mockCategory);
+
+		ResponseEntity<?> result = categories.getCategoryById(categoryId);
+
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertEquals(mockCategory, result.getBody());
+	}
+
+	@Test
+	public void testUpdateCategory_Success() {
+
+		CategoryRequest categoryToUpdate = categoryData();
+		CategoryResponse mockUpdatedCategory = new CategoryResponse(1L, "Spring Core",
+				"This is Spring Core Category Created");
+		when(categoryService.updateCategory(categoryToUpdate)).thenReturn(mockUpdatedCategory);
+
+		ResponseEntity<CategoryResponse> result = categories.updateCategory(categoryToUpdate);
+
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+		assertEquals(mockUpdatedCategory, result.getBody());
+	}
+
+	@Test
+	public void testDeleteCategory_Success() {
+
+		Long categoryId = 1L;
+		ResponseEntity<?> result = categories.deleteCategory(categoryId);
+		assertEquals(HttpStatus.OK, result.getStatusCode());
+	}
+
+	@Test
+	public void testGetCategoryById_CategoryNotFound() {
+
+		Long categoryId = 1L;
+		when(categoryService.getCategoryById(categoryId))
+				.thenThrow(new CategoryNotFoundException("Category not found"));
+		try {
+			ResponseEntity<?> result = categories.getCategoryById(categoryId);
+			assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
+		} catch (CategoryNotFoundException e) {
+		}
+	}
+
+	@Test
+	public void testUpdateCategory_CategoryNotFound() {
+
+		CategoryRequest categoryToUpdate = new CategoryRequest();
+		when(categoryService.updateCategory(categoryToUpdate))
+				.thenThrow(new CategoryNotFoundException("Category not found"));
+
+		try {
+			ResponseEntity<CategoryResponse> result = categories.updateCategory(categoryToUpdate);
+			assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
+		} catch (CategoryNotFoundException e) {
+
+		}
+	}
+
+	@Test
+	public void testDeleteCategory_CategoryNotFound() {
+
+		Long categoryId = 1L;
+		doThrow(new CategoryNotFoundException("Category not found")).when(categoryService).deleteCategory(categoryId);
+
+		try {
+			ResponseEntity<?> result = categories.deleteCategory(categoryId);
+			assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatusCode());
+		} catch (CategoryNotFoundException e) {
+
+		}
+
+	}
+}
